53936aa74adf3b3c0117e05189230561cf70ad8c
divitagulati <gulatidivita1006@gmail.com>
2021-07-16 15:10:36 +0530

    social-media-links-microwebsite

---
+++ index.js
@@ -0,0 +1,9 @@
+function eventFunction(){
+  document.getElementById("links").style.display="none";
+  document.getElementById("events").style.display="flex";
+}
+
+function linksFunction(){
+  document.getElementById("links").style.display="grid";
+  document.getElementById("events").style.display="none";
+}

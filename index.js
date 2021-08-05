df9bc72995df51674f864b2d1cda6e687aa0772c
Shlok Kaushik <shlokkaushik23@gmail.com>
2021-08-05 15:02:27 +0530

    update

--- index.js
+++ index.js
@@ -8,34 +8,3 @@ function linksFunction(){
   document.getElementById("events").style.display="none";
 }
 
-// const setTheme = theme => document.documentElement.className = theme;
-
-// let elements=document.querySelectorAll(".menu-item").length;
-
-// document.getElementById("color1").addEventListener('click', function() {
-// setTheme("color1");
-// });
-// document.getElementById("color2").addEventListener('click', function() {
-// setTheme("color2");
-// });
-// document.getElementById("color3").addEventListener('click', function() {
-// setTheme("color3");
-// });
-// document.getElementById("color4").addEventListener('click', function() {
-// setTheme("color4");
-// });
-// document.getElementById("color5").addEventListener('click', function() {
-// setTheme("color5");
-// });
-// document.getElementById("color6").addEventListener('click', function() {
-// setTheme("color6");
-// });
-              
-
-
-$(document).ready(function() {
-  $('.theme-colors li button').click(function(){
-    $('.btn').css('border', $(this).css('color'));
-  });
-
-});
